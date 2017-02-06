b5c8b68fd74ad380a9579675c2b06e7ecc5cd1ec
Dan Jordan <dan.jordan@highspeedtraining.co.uk>
2017-02-06 13:21:41 +0000

    delay emitting onOpen event

--- src/Modal.ts
+++ src/Modal.ts
@@ -133,13 +133,16 @@ export class Modal {
     // -------------------------------------------------------------------------
 
     open(...args: any[]) {
-        if (this.isOpened)
-            return;
+        if (this.isOpened) return;
         
         this.isOpened = true;
-        this.onOpen.emit(args);
         document.body.appendChild(this.backdropElement);
-        window.setTimeout(() => this.modalRoot.nativeElement.focus(), 0);
+
+        window.setTimeout(() => {
+            this.modalRoot.nativeElement.focus();
+            this.onOpen.emit(args);
+        }, 0);
+
         document.body.className += " modal-open";
     }
 
@@ -147,10 +150,11 @@ export class Modal {
         if (!this.isOpened)
             return;
 
-        this.isOpened = false;
-        this.onClose.emit(args);
         document.body.removeChild(this.backdropElement);
         document.body.className = document.body.className.replace(/modal-open\b/, "");
+
+        this.isOpened = false;
+        this.onClose.emit(args);
     }
 
     // -------------------------------------------------------------------------
